9a369bd683cec360fc568fa183bf4e1f35630ad0
Ricardo Terán <39249155+Rikardo816@users.noreply.github.com>
2021-02-24 15:18:41 -0500

    clase JefeEncargado creada (#4)

---
+++ src/proyectoiib_coders/JefeEncargado.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoiib_coders;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class JefeEncargado extends AgenciaAeronautica{
+    private String claveAcceso;
+
+    public String getClaveAcceso() {
+        return claveAcceso;
+    }
+
+    public void setClaveAcceso(String claveAcceso) {
+        this.claveAcceso = claveAcceso;
+    }
+    
+}
